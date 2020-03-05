747c40a7d6f89cfcc0be46cddb8e96346b955686
Jesse Callahan <jessetylercallahan@gmail.com>
2020-03-04 23:43:26 -0800

    adds planetLifeExp test, needs some work, almost done

--- __tests__/triangle.test.js
+++ __tests__/triangle.test.js
@@ -4,31 +4,35 @@ describe('Person', () => {
   var person;
 
   beforeEach(() => {
-    person = new Person(84, "CAN");
+    person = new Person(10, "US");
   });
 
   test('should correctly a person object with age', () => {
-    expect(person.age).toEqual(1);
+    expect(person.age).toEqual(10);
   });
 
   test('should return person age in Mercury years', () => {
-    expect(person.mercuryAge()).toEqual(4.166666666666667)
+    expect(person.mercuryAge()).toEqual(41)
   });
 
   test('should return person age in Venus years', () => {
-    expect(person.venusAge()).toEqual(1.6129032258064515)
+    expect(person.venusAge()).toEqual(16)
   });
 
   test('should return person age in Mars years', () => {
-    expect(person.marsAge()).toEqual(1.88)
+    expect(person.marsAge()).toEqual(18)
   });
 
   test('should return person age in Jupiter years', () => {
-    expect(person.jupiterAge()).toEqual(11.86)
+    expect(person.jupiterAge()).toEqual(118)
   });
 
   test('should return person life expectancy based on where they live', () => {
-    expect(person.lifeExpectancy()).toEqual(1.70)
+    expect(person.lifeExpectancy()).toEqual(78)
+  });
+
+  test('should return person life expectancy based on where they live', () => {
+    expect(person.planetsLifeExp()).toEqual(78)
   });
 
 });
--- src/solar.js
+++ src/solar.js
@@ -6,42 +6,52 @@ export class Person {
 
   mercuryAge() {
     this.age *= 100 / 24;
-    return this.age
+    return Math.floor(this.age);
   }
 
   venusAge() {
     this.age *= 100 / 62;
-    return this.age
+    return Math.floor(this.age);
   }
 
   marsAge() {
     this.age *= 1.88;
-    return this.age
+    return Math.floor(this.age);
   }
   jupiterAge() {
     this.age *= 11.86;
-    return this.age
+    return Math.floor(this.age);
   }
   lifeExpectancy() {
-    let a = this.age - 82.3;
+    let a = this.age - 80.96;
+    let b = this.age - 78.96;
+    let c = this.age - 82.30;
     if (this.age <= 83 && this.home === "UK") {
-      return 80.96
+      return Math.floor(80.96)
     }
     else if (this.age >= 81 && this.home === "UK") {
-      return this.age - 80.96
-    }
-    else if (this.age <= 83 && this.home === "US") {
-      return 78.96
-    }
-    else if (this.age >= 79 && this.home === "US") {
-      return this.age - 78.96
-    }
-    else if (this.age <= 83 && this.home === "CAN") {
-      return 82.3
-    }
-    else if (this.age >= 83 && this.home === "CAN") {
       return parseFloat(a.toFixed(1));
     }
+    else if (this.age <= 83 && this.home === "US") {
+      return Math.floor(78.96)
+    }
+    else if (this.age >= 79 && this.home === "US") {
+      return parseFloat(b.toFixed(1));
+    }
+    else if (this.age <= 83 && this.home === "CAN") {
+      return Math.floor(82.30)
+    }
+    else if (this.age >= 83 && this.home === "CAN") {
+      return parseFloat(c.toFixed(1));
+    }
+  }
+  planetsLifeExp() {
+    let a = this.lifeExpectancy();
+    let MercLifeExp = Math.floor(a * 4.16)
+    let VenusLifeExp = Math.floor(a * 1.61)
+    return [MercLifeExp, VenusLifeExp]
+
+
   }
 }
 
